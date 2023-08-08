f7d11ad751e30aef30273ade1861662b0e11c034
Osamagamal1911 <73962760+Osamagamal1911@users.noreply.github.com>
2023-08-08 20:08:26 +0300

    Update create_db.sh

--- create_db.sh
+++ create_db.sh
@@ -8,13 +8,13 @@ else
         pattern="^[^0-9\/><\?*\#'\@\$\%\^\&\(\)\.]+$"
 
         if [ -z $name ]; then
-                echo "\e[31mERROR: Name should not be null.\e[0m"
+                echo -e "\e[31mERROR: Name should not be null.\e[0m"
         elif [[ $name =~ ^(\.|\.\.) ]]; then
-                echo "\e[31mERROR: should not start with '.' or '..'.\e[0m"
+                echo -e "\e[31mERROR: should not start with '.' or '..'.\e[0m"
         elif [[ $name =~ ^[0-9] ]]; then
-                echo "\e[31mERROR: Name should not start with a number.\e[0m"
+                echo -e "\e[31mERROR: Name should not start with a number.\e[0m"
         elif [[ ! $name =~ $pattern ]]; then
-                echo "\e[31mERROR: Name contains invalid characters or spaces.\e[0m"
+                echo -e "\e[31mERROR: Name contains invalid characters or spaces.\e[0m"
         else
                 mkdir ./DB/$name 
         fi
