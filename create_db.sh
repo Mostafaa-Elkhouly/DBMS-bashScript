0e49d2711525761f9262ba93f9c9555c1b162b99
osama <g.osama1553@gmail.com>
2023-08-10 18:22:18 +0200

    update create_db.sh

--- create_db.sh
+++ create_db.sh
@@ -3,23 +3,17 @@
 while true; do
     read -r -p "Enter DB Name: " name
 
-    if [[ -e "./DB/$name" ]]; then
-        echo -e "\e[31mERROR: DB Exist!\e[0m"
-    else
-        pattern="^[^0-9\/><\?*\#'\@\$\%\^\&\(\)\.]+$"
+    rgx="^[A-Za-z_][A-Za-z0-9_]+$"
 
-        if [ -z "$name" ]; then
-            echo -e "\e[31mERROR: Name should not be null.\e[0m"
-        elif [[ $name =~ ^(\.|\.\.) ]]; then
-            echo -e "\e[31mERROR: should not start with '.' or '..'.\e[0m"
-        elif [[ $name =~ ^[0-9] ]]; then
-            echo -e "\e[31mERROR: Name should not start with a number.\e[0m"
-        elif [[ ! $name =~ $pattern ]]; then
-            echo -e "\e[31mERROR: Name contains invalid characters or spaces.\e[0m"
+    if [[ $name =~ $rgx ]]; then
+        if [[ -e "./DB/$name" ]]; then
+            echo -e "\e[31mERROR: DB Exist!\e[0m"
         else
             mkdir "./DB/$name"
-            echo -e "\e[32mSuccess: Database '$name' created.\e[0m"  
-            break  
+            echo -e "\e[32mSuccess: Database '$name' created.\e[0m"
+            break
         fi
+    else
+        echo -e "\e[31mERROR: Invalid DB Name!\e[0m"
     fi
 done
