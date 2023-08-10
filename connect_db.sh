d2f1221e89e8578282ac29c1048817e1cdc21460
osama <g.osama1553@gmail.com>
2023-08-10 17:57:43 +0200

    update create_db.sh

--- connect_db.sh
+++ connect_db.sh
@@ -1,14 +1,13 @@
-#! /usr/bin/bash
+#!/usr/bin/bash
 
-read -r -p "Enter DB Name: " name
+while true; do
+    read -r -p "Enter DB Name: " name
 
-if [[ -d "./DB/$name" && -e "./DB/$name" ]] ; then
-
-            clear
-            ./tablemenu.sh $name
-else
-    echo -e "\e[31mERROR: DB Not Found!\e[0m"
- 
-fi
-
-
+    if [[ -d "./DB/$name" && -e "./DB/$name" ]]; then
+        clear
+        ./tablemenu.sh "$name"
+        break
+    else
+        echo -e "\e[31mERROR: DB Not Found!\e[0m"
+    fi
+done
