aa8c5fdc5cf8ffe6248cdf70e131fe7bfd09af19
osama <g.osama1553@gmail.com>
2023-08-10 17:48:59 +0200

    update create_db.sh

--- create_db.sh
+++ create_db.sh
@@ -1,22 +1,25 @@
-#! /usr/bin/bash
+#!/usr/bin/bash
 
-read -r -p "Enter DB Name: " name
+while true; do
+    read -r -p "Enter DB Name: " name
 
-if [[ -e "./DB/$name" ]] ; then
+    if [[ -e "./DB/$name" ]]; then
         echo -e "\e[31mERROR: DB Exist!\e[0m"
-else
+    else
         pattern="^[^0-9\/><\?*\#'\@\$\%\^\&\(\)\.]+$"
 
-        if [ -z $name ]; then
-                echo -e "\e[31mERROR: Name should not be null.\e[0m"
+        if [ -z "$name" ]; then
+            echo -e "\e[31mERROR: Name should not be null.\e[0m"
         elif [[ $name =~ ^(\.|\.\.) ]]; then
-                echo -e "\e[31mERROR: should not start with '.' or '..'.\e[0m"
+            echo -e "\e[31mERROR: should not start with '.' or '..'.\e[0m"
         elif [[ $name =~ ^[0-9] ]]; then
-                echo -e "\e[31mERROR: Name should not start with a number.\e[0m"
+            echo -e "\e[31mERROR: Name should not start with a number.\e[0m"
         elif [[ ! $name =~ $pattern ]]; then
-                echo -e "\e[31mERROR: Name contains invalid characters or spaces.\e[0m"
+            echo -e "\e[31mERROR: Name contains invalid characters or spaces.\e[0m"
         else
-                mkdir ./DB/$name 
+            mkdir "./DB/$name"
+            echo -e "\e[32mSuccess: Database '$name' created.\e[0m"  
+            break  
         fi
-fi
-
+    fi
+done
