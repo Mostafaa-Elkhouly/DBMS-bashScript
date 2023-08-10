62a57ef3b77e5990af8b44d557d994e2a45b2943
osama <g.osama1553@gmail.com>
2023-08-10 18:50:08 +0200

    update create_table.sh

--- Create_table.sh
+++ Create_table.sh
@@ -1,43 +1,38 @@
-#! /usr/bin/bash
+#!/usr/bin/bash
 
 dbname=$1
 cd "./DB/$dbname/"
 
 function isInteger
-{   
-
-    intPattern="^[0-9]+$"
+{
+    intPattern="^[0-9]+*$"
     if [[ $1 =~ $intPattern ]]
     then
         return 0
-    else 
+    else
         return 1
     fi
 }
 
-
-
 function promptForPK
 {
-    echo "Make Current Column The Primary Key? "    
+    echo "Make Current Column The Primary Key? "
     select ans in "yes" "no"
-    do 
-        case $REPLY in 
-        1)  return 0
-            ;;
-        2)  return 1
-            ;;
-        *) echo -e "\e[31mERROR: Invalid Option\e[0m"    
+    do
+        case $REPLY in
+            1)  return 0
+                ;;
+            2)  return 1
+                ;;
+            *) echo -e "\e[31mERROR: Invalid Option\e[0m"
         esac
-        
     done
-
 }
-
 
 function isValidString
 {
-    if [[ $1 =~ ^[a-zA-Z0-9]+$ ]]
+    rgx="^[A-Za-z_][A-Za-z0-9_]+*$"
+    if [[ $1 =~ $rgx ]]
     then
         return 0
     else
@@ -45,70 +40,72 @@ function isValidString
     fi
 }
 
-
 function createTable
-{   
-    read -r -p "Enter Table Name--> " tableName
-  
-    if [ -f $tableName ]
-    then 
-        echo -e "\e[31mERROR: Table Name Already Exsits!\e[0m"
-        return 1
-    fi
+{
+    while true; do
+        read -r -p "Enter Table Name--> " tableName
 
-    read -r -p "Enter Columns Number--> " columnsNumber
-    
-    while ! isInteger $columnsNumber
-    do 
-        read -r -p "INVALID VALUE! `echo $'\n '` Enter Columns Number--> " columnsNumber        
-    done
-
-
-    typeset -i counter=0
-    columnSchema=""
-    primKey=''
-    while  [[ counter -lt columnsNumber ]]
-    do
-        currCol=$(( counter+1 ))
-        
-        read -r -p "Enter Column [$currCol] Name--> " colName
-        echo "Choose The Column [$currCol] Type: "
-        select type in "int" "string"
-        do
-            case $REPLY in 
-                1|2) colType=$type
-                    break ;;
-                *) echo -e "\e[31mERROR: Invalid Option\e[0m"
-            esac
-        done 
-        
-        if [ -z $primKey ]
-        then
-            promptForPK
-            if [ $? -eq 0 ] 
-            then 
-                primKey=$colName
-                colName+="(PK)"
-            fi  
+        if [ -f "$tableName" ]; then
+            echo -e "\e[31mERROR: Table Name Already Exists!\e[0m"
+            continue
+        elif ! isValidString "$tableName"; then
+            echo -e "\e[31mERROR: Invalid Table Name!\e[0m"
+            continue
         fi
 
-        if [[ counter -eq columnsNumber-1 ]]; then
-            columnSchema+="$colName:$colType"
-        else    
-            columnSchema+="$colName:$colType|"
-        fi
-        
-        ((counter++))
-    done
+        read -r -p "Enter Columns Number--> " columnsNumber
 
-     
-    echo $columnSchema >> "./$tableName"
-    if [[ $? == 0 ]]
-    then
-        echo "Table $tableName was created successfully!"
-    else
-        echo -e "\e[31mERROR: Creating Table\e[0m"
-    fi
+        while ! isInteger "$columnsNumber"; do
+            read -r -p "INVALID VALUE! `echo $'\n '` Enter Columns Number--> " columnsNumber
+        done
+
+        typeset -i counter=0
+        columnSchema=""
+        primKey=''
+
+        while [[ counter -lt columnsNumber ]]; do
+            currCol=$(( counter+1 ))
+
+            read -r -p "Enter Column [$currCol] Name--> " colName
+            if ! isValidString "$colName"; then
+                echo -e "\e[31mERROR: Invalid Column Name!\e[0m"
+                continue
+            fi
+
+            echo "Choose The Column [$currCol] Type: "
+            select type in "int" "string"; do
+                case $REPLY in
+                    1|2) colType=$type
+                        break ;;
+                    *) echo -e "\e[31mERROR: Invalid Option\e[0m"
+                esac
+            done
+
+            if [ -z "$primKey" ]; then
+                promptForPK
+                if [ $? -eq 0 ]; then
+                    primKey=$colName
+                    colName+="(PK)"
+                fi
+            fi
+
+            if [[ counter -eq columnsNumber-1 ]]; then
+                columnSchema+="$colName:$colType"
+            else
+                columnSchema+="$colName:$colType|"
+            fi
+
+            ((counter++))
+        done
+
+        echo "$columnSchema" >> "./$tableName"
+        if [[ $? -eq 0 ]]; then
+            echo -e "\e[32mTable $tableName was created successfully!\e[0m"
+            break
+        else
+            echo -e "\e[31mERROR: Creating Table\e[0m"
+        fi
+    done
 }
 
 createTable
