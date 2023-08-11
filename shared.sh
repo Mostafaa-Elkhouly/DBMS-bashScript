4cf948572c9e19722fab5a907b736f8319d7fe0a
Mostafaa-Elkhouly <ms.elkhouly97@gmail.com>
2023-08-11 17:46:12 +0300

    edit insert_table.sh with shared.sh

--- shared.sh
+++ shared.sh
@@ -33,6 +33,25 @@ function getPK {
     echo ${pk[@]}
 }
 
+function checkForUniqueInPK {
+
+    columnvalues=($( awk -F "|" -v colnum="$2" '{
+        if(NR > 1)
+        {
+            print $colnum
+        }
+    }' $3
+    ))
+
+    for value in ${columnvalues[@]}
+    do
+        if [[ $1 -eq $value ]]; then
+            echo -e "\e[31mERROR: You Must Enter a Unique Value For the PK Column\e[0m"
+            return 1
+        fi
+    done
+}
+
 function colnames {
 
     column_names=(
@@ -50,3 +69,37 @@ function colnames {
     echo ${column_names[@]}
 }
 
+function coltypes {
+
+    column_types=(
+    $(
+        awk -F "|" '
+        {
+            if(NR == 1){
+                for (i = 1; i <= NF; i++) {
+                    split($i, parts, ":")
+                    print parts[2]
+                }
+            }
+        }' ./$1))
+    
+    echo ${column_types[@]}
+}
+
+function isInteger {   
+    intPattern="^[0-9]+$"
+    if [[ $1 =~ $intPattern ]]; then
+        echo 0
+    else 
+        echo 1
+    fi
+}
+
+function isValidString {
+    if [[ $1 =~ ^[a-zA-Z0-9]+$ ]]
+    then
+        echo 0
+    else
+        echo 1
+    fi
+}
