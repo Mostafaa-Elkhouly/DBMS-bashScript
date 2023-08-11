0690f51d2ff76148e5357e9d2f78653455241240
Mostafaa-Elkhouly <ms.elkhouly97@gmail.com>
2023-08-11 17:40:55 +0300

    edit select_table.sh with shared.sh

--- select_table.sh
+++ select_table.sh
@@ -1,58 +1,10 @@
 #! /usr/bin/bash
+source ./shared.sh
 
 dbname=$1
 cd "./DB/$dbname/"
 export PS3="$dbname> "
 
-
-function getPK {
-    pk=(
-        $( 
-            awk '
-            BEGIN{FS="|"}
-            {
-                if(NR == 1)
-                {
-                    i=1
-                    while(i<=NF){ 
-                        split($i, column, ":")
-                        column_name = column[1]
-                        column_type = column[2]
-
-                        if ( column_name ~ /(PK)/ )
-                        {
-                            print column_name
-                            print i 
-                            break
-                        }
-                        i++
-                    }
-                }
-            }
-            ' ./$1
-        )
-    )
-    
-    echo ${pk[@]}
-}
-
-
-function colnames {
-
-    column_names=(
-    $(
-        awk -F "|" '
-        {
-            if(NR == 1){
-                for (i = 1; i <= NF; i++) {
-                    split($i, parts, ":")
-                    print parts[1]
-                }
-            }
-        }' ./$1))
-    
-    echo ${column_names[@]}
-}
 
 function selectFromTable
 {   
--- shared.sh
+++ shared.sh
@@ -2,3 +2,51 @@
 
 #all shared functinons goes here 
 
+function getPK {
+    pk=(
+        $( 
+            awk '
+            BEGIN{FS="|"}
+            {
+                if(NR == 1)
+                {
+                    i=1
+                    while(i<=NF){ 
+                        split($i, column, ":")
+                        column_name = column[1]
+                        column_type = column[2]
+
+                        if ( column_name ~ /(PK)/ )
+                        {
+                            print column_name
+                            print i 
+                            break
+                        }
+                        i++
+                    }
+                }
+            }
+            ' ./$1
+        )
+    )
+    
+    echo ${pk[@]}
+}
+
+function colnames {
+
+    column_names=(
+    $(
+        awk -F "|" '
+        {
+            if(NR == 1){
+                for (i = 1; i <= NF; i++) {
+                    split($i, parts, ":")
+                    print parts[1]
+                }
+            }
+        }' ./$1))
+    
+    echo ${column_names[@]}
+}
+
