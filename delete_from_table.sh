a587c6a94847f6657cc9e56d385d971c3a1e3ce3
Mostafaa-Elkhouly <ms.elkhouly97@gmail.com>
2023-08-12 22:59:01 +0300

    update delete_from_table.sh

--- delete_from_table.sh
+++ delete_from_table.sh
@@ -23,44 +23,19 @@ function deleteFromTable
         read -r -p "Enter value of ${pk[0]} : " val
         pk_index=${pk[1]}
 
-        if [[ ${column_types[$pk_index]} = "int" ]]; then
+        check_if_pk_exist=$(cut -d'|' -f ${pk[1]} ./$tableName | sed "1d" | grep -w "$val")
+        idval=$val
 
+        while [[ -z "$check_if_pk_exist" ]]
+        do
+            if [ "$(isInteger "$val")" -eq 1 ]; then
+            
+                echo -e "\e[31mERROR: PK not Found, Please Try Again ...\e[0m"
+            fi
+            read -r -p "Enter value of ${pk[0]} : " val
             check_if_pk_exist=$(cut -d'|' -f ${pk[1]} ./$tableName | sed "1d" | grep -w "$val")
             idval=$val
-
-            while [[ "$(isInteger "$val")" -eq 1 || -z "$check_if_pk_exist" ]]
-            do
-                if [ "$(isInteger "$val")" -eq 1 ]; then
-
-                    echo -e "\e[31mERROR: You must enter an integer number, please try again ...\e[0m"
-
-                elif [ -z "$check_if_pk_exist" ]; then
-
-                    echo -e "\e[31mERROR: PK not Found, Please Try Again ...\e[0m"
-                fi
-                read -r -p "Enter value of $column : " val
-                check_if_pk_exist=$(cut -d'|' -f ${pk[1]} ./$tableName | sed "1d" | grep -w "$val")
-                idval=$val
-            done
-        else 
-            check_if_pk_exist=$(cut -d'|' -f ${pk[1]} ./$tableName | sed "1d" | grep -w "$val")
-            idval=$val
-
-            while [[ "$(isValidString "$val")" -eq 1 || -z "$check_if_pk_exist" ]]
-            do
-                if [ "$(isValidString "$val")" -eq 1 ]; then
-
-                    echo -e "\e[31mERROR: You must enter an integer number, please try again ...\e[0m"
-
-                elif [ -z "$check_if_pk_exist" ]; then
-
-                    echo -e "\e[31mERROR: PK not Found, Please Try Again ...\e[0m"
-                fi                                   
-                read -r -p "Enter value of $column : " val
-                check_if_pk_exist=$(cut -d'|' -f ${pk[1]} ./$tableName | sed "1d" | grep -w "$val")
-                idval=$val
-            done
-        fi
+        done
 
         #get record number of PK
         col_num=${pk[1]}
